f6d6906104d23ab75ad5df36c7cb114ed327ebe3
yuzhao <yuzhao5@illinois.edu>
2019-11-22 01:28:29 -0600

    1. add a few textviews and one button to the activity_main.xml 2. get google map api

--- app/src/main/java/com/example/finalproject/MainActivity.java
+++ app/src/main/java/com/example/finalproject/MainActivity.java
@@ -1,17 +1,33 @@
 package com.example.finalproject;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+
+public class MainActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private com.google.android.gms.maps.GoogleMap map;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        final Button addEvent = findViewById(R.id.addEvent);
 
+        addEvent.setOnClickListener(unused -> addClicked());
+
+    }
+    private void addClicked() {
+
+    }
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
 
     }
 }
