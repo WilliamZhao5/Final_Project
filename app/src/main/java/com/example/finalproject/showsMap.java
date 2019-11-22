efcbbf1af3ec8c4d41d401b7d2673f083d3002d2
yuzhao <yuzhao5@illinois.edu>
2019-11-22 14:24:08 -0600

    1. add a few textviews and one button to the activity_main.xml 2. get google map api done 3. zoom in (map)

---
+++ app/src/main/java/com/example/finalproject/showsMap.java
@@ -0,0 +1,44 @@
+package com.example.finalproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentActivity;
+
+import android.location.Location;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class showsMap extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_shows_map);
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney, Australia, and move the camera.
+        LatLng location = new LatLng(40.104869, -88.227146);
+        final float defaultMapZoom = 17f;
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(
+                new LatLng(location.latitude, location.longitude), defaultMapZoom));
+        mMap.addMarker(new MarkerOptions().position(location).title("Marker in UIUC"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(location));
+    }
+}
