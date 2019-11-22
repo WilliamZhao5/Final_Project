7068dcd04127cce37864caf9b1f3922ac9391fb1
yuzhao <yuzhao5@illinois.edu>
2019-11-22 16:07:24 -0600

    1. add a few textviews and one button to the activity_main.xml 2. get google map api done 3. zoom in (map) 4. set private variables

--- app/src/main/java/com/example/finalproject/MainActivity.java
+++ app/src/main/java/com/example/finalproject/MainActivity.java
@@ -7,7 +7,9 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -18,14 +20,27 @@ import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MainActivity extends AppCompatActivity {
 
-    private com.google.android.gms.maps.GoogleMap map;
-
-    private GoogleMap mMap;
+    private String eventName;
+    private String locationName;
+    private int time;
+    private int date;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        EditText event = findViewById(R.id.eventName);
+        eventName = event.getText().toString();
+
+        EditText location = findViewById(R.id.eventLocation);
+        locationName = location.getText().toString();
+
+        EditText eventTime = findViewById(R.id.eventTime);
+        time = Integer.parseInt(eventTime.getText().toString());
+
+        EditText eventDate = findViewById(R.id.eventDate);
+        date = Integer.parseInt(eventDate.getText().toString());
 
         final Button addEvent = findViewById(R.id.addEvent);
 
