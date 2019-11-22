bc7d440096c5d99179928ddedb5616f16230805d
yuzhao <yuzhao5@illinois.edu>
2019-11-22 02:21:54 -0600

    1. add a few textviews and one button to the activity_main.xml 2. get google map api

--- app/src/main/java/com/example/finalproject/MainActivity.java
+++ app/src/main/java/com/example/finalproject/MainActivity.java
@@ -3,15 +3,24 @@ package com.example.finalproject;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MainActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private com.google.android.gms.maps.GoogleMap map;
+
+    private GoogleMap mMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,11 +32,20 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
         addEvent.setOnClickListener(unused -> addClicked());
 
     }
-    private void addClicked() {
 
-    }
     @Override
     public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
 
+        // Add a marker in Sydney, Australia, and move the camera.
+        LatLng uiuc = new LatLng(40.101887, -88.227376);
+        mMap.addMarker(new MarkerOptions().position(uiuc).title("Marker in UIUC"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(uiuc));
+    }
+
+    private void addClicked() {
+        Intent intent = new Intent(this, showsMap.class);
+        startActivity(intent);
+        finish();
     }
 }
