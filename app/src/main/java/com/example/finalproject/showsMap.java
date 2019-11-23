1b0e0d2ca7258e69d3c29e89864174ed97772012
yuzhao <yuzhao5@illinois.edu>
2019-11-22 19:40:43 -0600

    1. add a few textviews and one button to the activity_main.xml 2. get google map api done 3. zoom in (map) 4. set private variables

--- app/src/main/java/com/example/finalproject/showsMap.java
+++ app/src/main/java/com/example/finalproject/showsMap.java
@@ -2,7 +2,16 @@ package com.example.finalproject;
 
 import androidx.fragment.app.FragmentActivity;
 
+import android.location.Address;
+import android.location.Geocoder;
+import android.nfc.Tag;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.inputmethod.EditorInfo;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -11,17 +20,63 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class showsMap extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+
+    private EditText mSearchText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_shows_map);
+
+        mSearchText = findViewById(R.id.input_search);
+
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+    }
+
+    private void init() {
+        mSearchText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent keyEvent) {
+
+                if (actionId == EditorInfo.IME_ACTION_SEARCH
+                        || actionId == EditorInfo.IME_ACTION_DONE
+                        || keyEvent.getAction() == keyEvent.ACTION_DOWN
+                        || keyEvent.getAction() == keyEvent.KEYCODE_ENTER) {
+                    //call searching
+                    geoLocate();
+
+                }
+                return false;
+            }
+        });
+
+    }
+
+    private void geoLocate() {
+        String searchString = mSearchText.getText().toString();
+
+        Geocoder geocoder = new Geocoder(showsMap.this);
+        List<Address> list = new ArrayList<>();
+        try {
+            list = geocoder.getFromLocationName(searchString, 1);
+        } catch (IOException e) {
+            Log.e("gg", "geolocate: IOException: " + e.getMessage());
+        }
+
+        if (list.size() > 0) {
+            Address address = list.get(0);
+            Log.d("PP", "found location: " + address.toString());
+            //Toast.makeText(this, address.toString(), Toast.LENGTH_SHORT).show();
+        }
     }
 
     @Override
@@ -35,5 +90,7 @@ public class showsMap extends FragmentActivity implements OnMapReadyCallback {
                 new LatLng(location.latitude, location.longitude), defaultMapZoom));
         mMap.addMarker(new MarkerOptions().position(location).title("Marker in UIUC"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(location));
+
+        init();
     }
 }
