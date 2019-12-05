5246cf58f47aca1130324d9610dd585c00e6db46
yuzhao <yuzhao5@illinois.edu>
2019-12-05 00:36:30 -0600

    Email done!!!

--- app/src/main/java/com/example/finalproject/Utils.java
+++ app/src/main/java/com/example/finalproject/Utils.java
@@ -2,5 +2,5 @@ package com.example.finalproject;
 
 public class Utils {
     public static final String EMAIL = "yuzhao5inno@gmail.com";
-    public static final String PASSWORD = "";
+    public static final String PASSWORD = "INnovation!";
 }
